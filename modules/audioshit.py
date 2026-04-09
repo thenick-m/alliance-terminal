4f067853ca6cb4d9370bc76d40c09967e73058cf
thenick_m <nicholasmetaverso@gmail.com>
2026-04-09 09:35:50 -0700

    radio server infrastructure added

--- modules/audioshit.py
+++ modules/audioshit.py
@@ -9,19 +9,19 @@ _active_sfx = []
 # --- SFX ---
 
 def play_sound(filename, volume=None, max_time=None):
-    vol = volume if volume is not None else sfx_volume
+    vol = sfx_volume if volume is None else volume
 
     #clean up finished sounds
     _active_sfx[:] = [p for p in _active_sfx if p.playing]
 
     p = Playback()
     p.load_file(filename)
-    p.set_volume(vol)
+    p.set_volume(vol) #TODO: fix ts not changing
     p.play()
     _active_sfx.append(p)
 
     if max_time:
-        threading.Timer(max_time / 1000, p.stop).start()
+        threading.Timer(max_time/1000, p.stop).start()
 
     return p
 
